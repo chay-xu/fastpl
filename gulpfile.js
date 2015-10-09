3d3c4b4073933f8f31405a6045909c0b99e84a7e
changfeng <changfeng@xiaokakeji.com>
2015-10-09 17:42:22 +0800

    update version 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,56 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var cssmin = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
-var rev = require('gulp-rev');
 
-//var gifsicle = require('imagemin-gifsicle');
-//var pngcrush = require('imagemin-pngcrush')
 
 // 语法检查
 gulp.task('jshint', function () {
-    return gulp.src('dialog.js')
+    return gulp.src( 'src/*.js' )
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-var options = {
-    preserveComments: 'some'
-};
+// 清空文件夹
+gulp.task('clean', function(){
+  return gulp.src('build/', {read: false})
+      .pipe(clean());
+})
+
 // js压缩代码
-gulp.task('script', function (){
-    
-
-     return gulp.src('src/*.js')
-          // .pipe(concat('all.js'))
-          // .pipe(gulp.dest('dist'))
-          .pipe(uglify(options))
-          .pipe(rename({
-            suffix: "-min"
-          }))
-          .pipe(gulp.dest('build'));
+gulp.task('jsmin', function () {
+  return gulp.src( 'src/fastpl.js' )
+      .pipe(sourcemaps.init())
+      .pipe(uglify({
+        preserveComments: 'some'
+      }))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(sourcemaps.write('/'))
+      .pipe(gulp.dest('build'));
 });
-
-// css压缩代码
-gulp.task('css', function (){
-     return gulp.src('src/css/*.css')
-          // .pipe(concat('all.js'))
-          // .pipe(gulp.dest('dist'))
-          .pipe(cssmin({
-            keepSpecialComments: 1
-          }))
-          .pipe(rename({
-            suffix: "-min"
-          }))
-          .pipe(gulp.dest('build/css/'));
+gulp.task('jsplus', function () {
+  return gulp.src( ['src/fastpl.js', 'src/plus.js'] )
+      .pipe(concat('fastpl-plus.js'))
+      .pipe(sourcemaps.init())
+      .pipe(uglify({
+        preserveComments: 'some'
+      }))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(sourcemaps.write('/'))
+      .pipe(gulp.dest('build'));
 });
-
-// images压缩代码
-gulp.task('image', function (){
-     return gulp.src('src/images/*.gif')
-          // .pipe(concat('all.js'))
-          // .pipe(gulp.dest('dist'))
-          .pipe(gifsicle({
-            interlaced: true
-          }))
-          // .pipe(rename({
-          //   suffix: "-min"
-          // }))
-          .pipe(gulp.dest('build/images/'));
-});
-
-// 修改文件名
-gulp.task('rev', function () {
-    return gulp.src('src/*.js')
-          .pipe( rev() )
-          .pipe( gulp.dest('build') );
+gulp.task('js', ['clean'], function () {
+  gulp.start('jsmin', 'jsplus');
 });
 
 // 监视文件的变化
 gulp.task('watch', function () {
-    gulp.watch('src/*.js', ['jshint', 'minify']);
+    gulp.watch('src/*.js', ['jshint', 'js']);
 });
-
-// 压缩代码
-gulp.task('minify', ['script', 'css' ] );
-
-// 注册缺省任务
-gulp.task('default', ['jshint', 'minify', 'watch']);
-
-// 可以看出，基本上所有的任务体都是这么个模式：
-// gulp.task('任务名称', function () {
-//     return gulp.src('文件')
-//         .pipe(...)
-//         .pipe(...)
-//         // 直到任务的最后一步
-//         .pipe(...);
-// });
--- src/fastpl.js
+++ src/fastpl.js
@@ -2,8 +2,8 @@
  * @file js template
  * @author xucaiyu
  * @email 569455187@qq.com
- * @version 3.0
- * @date 2015-03-30
+ * @version 4.0
+ * @date 2015-10-09
  * @license MIT License 
  */
 (function() {
@@ -26,7 +26,7 @@
 
         if(typeof html !== 'string') return html;
 
-        return html.replace( /[&<>"']+/igm, escapeFn )
+        return html.replace( /[&<>"'/]/igm, escapeFn )
     }
 
     // 是否数组
@@ -37,14 +37,6 @@
     // each 循环
     var _each_ = function (data, min, max, callback) {
         var i, len;
-
-        // if not min and max
-        // if( typeof min === 'function' ){
-        //     i = 0;
-        //     len = data.length;
-        //     callback = min;
-        //     min = undefined;
-        // }
 
         if (isArray(data)) {
             i = min;
@@ -58,68 +50,10 @@
             }
         }
     };
-    // if判断是否存在值
-    function _ifEmpty_( val, newVal ){
-        if( val )
-            return val;
-        else
-            return newVal;
+    // 逻辑判断是否存在值
+    function _logical_( val ){
+        return val;
     }
-    // 日期时间格式化
-    /**
-      * 可以用 1-2 个占位符 * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) * eg: * (new
-        Date()).pattern("yyyy-MM-dd hh:mm:ss.S")==> 2006-07-02 08:09:04.423      
-      * (new Date()).pattern("yyyy-MM-dd E HH:mm:ss") ==> 2009-03-10 二 20:09:04      
-      * (new Date()).pattern("yyyy-MM-dd EE hh:mm:ss") ==> 2009-03-10 周二 08:09:04      
-      * (new Date()).pattern("yyyy-MM-dd EEE hh:mm:ss") ==> 2009-03-10 星期二 08:09:04      
-      * (new Date()).pattern("yyyy-M-d h:m:s.S") ==> 2006-7-2 8:9:4.18      
-      */ 
-    var date_format = function( date, fmt ){
-        if( !date ) return;
-
-        var dt = new Date( date.replace(/\-/g, '/') ),
-            o = {         
-                "M+" : dt.getMonth()+1, //月份         
-                "d+" : dt.getDate(), //日         
-                "h+" : dt.getHours()%12 == 0 ? 12 : dt.getHours()%12, //12小时制         
-                "H+" : dt.getHours(), //24小时制         
-                "m+" : dt.getMinutes(), //分         
-                "s+" : dt.getSeconds(), //秒         
-                "q+" : Math.floor((dt.getMonth()+3)/3), //季度         
-                "S" : dt.getMilliseconds() //毫秒         
-            };         
-        var week = {         
-            "0" : "\u65e5",         
-            "1" : "\u4e00",         
-            "2" : "\u4e8c",         
-            "3" : "\u4e09",         
-            "4" : "\u56db",         
-            "5" : "\u4e94",         
-            "6" : "\u516d"        
-        };         
-        if(/(y+)/.test(fmt)){         
-            fmt=fmt.replace(RegExp.$1, (dt.getFullYear()+"").substring(4 - RegExp.$1.length));        
-        }         
-        if(/(E+)/.test(fmt)){         
-            fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "\u661f\u671f" : "\u5468") : "")+week[dt.getDay()+""]);         
-        }         
-        for(var k in o){         
-            if(new RegExp("("+ k +")").test(fmt)){         
-                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substring((""+ o[k]).length)));         
-            }         
-        }
-                 
-        return fmt;
-    }
-    // 数字格式化 货币
-    var number_format = function( value ){
-
-        if( !value ){
-            return;
-        }else{
-            return String(value).replace( /\B(?=(?:\d{3})+$)/g, ',' )
-        }
-    };
 
     // 获取长度
     var count = function( o ){
@@ -149,10 +83,8 @@
     var tools = {
         _escape_: _escape_,
         _each_: _each_,
-        _ifEmpty_: _ifEmpty_,
-        count: count,
-        date_format: date_format,
-        number_format: number_format
+        _logical_: _logical_,
+        count: count
     }
 
     fasTpl.tools = function( name, fn ){
@@ -252,7 +184,7 @@
             variablePattern = new RegExp(variableReg, 'igm'),
             //noneencodePattern = new RegExp(escapeReg, 'igm'),
             commentPattern = new RegExp(commentReg, 'igm'),
-            headerCode = 'var _escape_=$tools._escape_,_each_=$tools._each_,_ifEmpty_=$tools._ifEmpty_,',
+            headerCode = 'var _escape_=$tools._escape_,_each_=$tools._each_,_logical_=$tools._logical_,',
             footerCode = '',
             list = {},
             view;
@@ -260,7 +192,9 @@
         // 声明自定义函数
         function concat( name ){
             var val;
+            // name不存在或者list中存在name
             if( !name || list[ name ] ) return;
+            // 添加name
             if( tools[ name ] ){
                 val = '$tools.' + name;
             }else{
@@ -281,7 +215,7 @@
                 headerCode += key + '=\'' + args + '\',';
                 return '\';_str+=' + key + '+\'';
             })
-            .replace( operationPattern, function(all, type, args ){
+            .replace( operationPattern, function(all, type, args){
                 // console.log(all, type,'---' , args,param);
                 // console.log( type, args )
                 var tag = fasTpl.statement[ type ];
@@ -292,25 +226,18 @@
                 // 逻辑
                 return '\'; '+ tag( args )+' _str+=\'';
             })
-            .replace( variablePattern, function(all, escape, value, symbol, args ){
-                // console.log(all, escape, type, custom );
+            .replace( variablePattern, function(all, escape, value, symbol, args){
+                // console.log(all, '--', escape, '--',value, '--', symbol, '--', args);
                 
                 // || 判断语句
-                if( symbol == '||' ){
+                if( symbol && symbol !== '|' ){
                     // 防止未定义的变量报错
                     if( value.indexOf( '.' ) === -1 ){
                         concat( value );
                     }
 
-                    value = '_ifEmpty_('+ value + ','+args+')';
-                }
-                // 转义变量
-                if( escape !== '=' ){
-                    value = '_escape_(' + value + ')'
-                }
-
-                // | 自定义方法
-                if( symbol == '|' ){
+                    value = '_logical_('+ value + symbol + args+')';
+                }else{
                     if( args ){
                         var split = args.split(':'),
                             name = split[0].replace(/\s/g, ''),
@@ -322,7 +249,11 @@
                         concat( name );
                     };
                 }
-// console.log( value )                
+                // 转义变量
+                if( escape !== '=' ){
+                    value = '_escape_(' + value + ')'
+                }
+               
                 return '\'+'+value+ '+\'';
             })
             .replace(/[\r\t\n]/g,'');
@@ -342,7 +273,6 @@
             })
             // 组合代码
             code = headerCode + dataCode + footerCode;
-            // console.log( code );
             return (new Function( '_data, $tools', code ))(data, tools);
         }
     }
